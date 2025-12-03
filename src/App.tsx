8632badb24a4e5ffbcb909dfc512f8cc1c70e651
gb05313594187-tech <gb05313594187@gmail.com>
2025-12-03 22:20:37 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,19 @@
 // @ts-nocheck
-/* eslint-disable */
-import { Toaster } from "@/components/ui/sonner";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "@/contexts/AuthContext";
-import { LanguageProvider } from "@/contexts/LanguageContext";
-import { FollowProvider } from "@/contexts/FollowContext";
-import Navbar from "@/components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "@/pages/Index";
+import CoachesPage from "@/pages/Coaches";
+import CompaniesPage from "@/pages/Companies";
 
-import Index from "./pages/Index";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import AdminPanel from "./pages/AdminPanel"; // ADMIN IMPORT
-import CoachList from "./pages/CoachList";
-import CoachProfile from "./pages/CoachProfile";
-import BookingSystem from "./pages/BookingSystem";
-import PaymentPage from "./pages/Payment";
-import PaymentSuccess from "./pages/PaymentSuccess";
-import MentorCircle from "./pages/MentorCircle";
-import ForCompanies from "./pages/ForCompanies";
-import Webinars from "./pages/Webinars";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <LanguageProvider>
-      <AuthProvider>
-        <FollowProvider>
-            <Toaster />
-            <BrowserRouter>
-              <div className="flex flex-col min-h-screen">
-                <Navbar />
-                <main className="flex-grow">
-                  <Routes>
-                    <Route path="/" element={<Index />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    
-                    {/* KULLANICI PANELİ */}
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    
-                    {/* YÖNETİCİ PANELİ (BURASI ÖNEMLİ) */}
-                    <Route path="/admin" element={<AdminPanel />} />
-                    
-                    <Route path="/coaches" element={<CoachList />} />
-                    <Route path="/coach/:id" element={<CoachProfile />} />
-                    <Route path="/booking/:id" element={<BookingSystem />} />
-                    <Route path="/payment/:id" element={<PaymentPage />} />
-                    <Route path="/payment-success" element={<PaymentSuccess />} />
-                    <Route path="/mentor-circle" element={<MentorCircle />} />
-                    <Route path="/corporate" element={<ForCompanies />} />
-                    <Route path="/webinars" element={<Webinars />} />
-                    <Route path="*" element={<NotFound />} />
-                  </Routes>
-                </main>
-              </div>
-            </BrowserRouter>
-        </FollowProvider>
-      </AuthProvider>
-    </LanguageProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-white">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/coaches" element={<CoachesPage />} />
+          <Route path="/companies" element={<CompaniesPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}

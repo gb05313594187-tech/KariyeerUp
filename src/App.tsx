b0f837657572c75cadb7824c22e442de41b429b6
gb05313594187-tech <gb05313594187@gmail.com>
2025-12-04 13:57:59 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 // @ts-nocheck
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "@/pages/Index";
-import forCoachesPage from "@/pages/forCoaches";
-import forCompaniesPage from "@/pages/forCompanies";
+
+import Index from "./pages/Index";
+import ForCoaches from "./pages/ForCoaches";
+import ForCompanies from "./pages/ForCompanies";
 
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-white">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/forcoaches" element={<CoachesPage />} />
-          <Route path="/forcompanies" element={<CompaniesPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/coaches" element={<ForCoaches />} />
+        <Route path="/companies" element={<ForCompanies />} />
+      </Routes>
     </Router>
   );
 }

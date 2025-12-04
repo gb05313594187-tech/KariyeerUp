164c0c14c9ebdc6926c0e4dfffd1fb806dade3bb
gb05313594187-tech <gb05313594187@gmail.com>
2025-12-04 13:16:52 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/", // Vercel'de kökten yayınlanacağı için
   build: {
-    outDir: "dist",
+    outDir: "dist", // Vercel Build Output Directory ile aynı olmalı
   },
 });

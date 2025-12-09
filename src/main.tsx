cedbc286813a87031477bed06e588a4fda27e54e
gb05313594187-tech <gb05313594187@gmail.com>
2025-12-09 16:27:09 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
-// src/main.tsx
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// LanguageProvider'ı context dosyandan import et
-// Dosyan büyük ihtimalle: src/context/LanguageContext.tsx
-// o zaman path böyle olmalı:
-import { LanguageProvider } from "@/context/LanguageContext";
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+    <App />
   </React.StrictMode>
 );

c1c01d5fb8bbdf647922129fddcc5ee4397fcbf4
gb05313594187-tech <gb05313594187@gmail.com>
2025-12-09 17:47:54 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// DİKKAT: contexts (sonunda S var)
+// Dikkat: contexts klasörü (s ile)
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import { AuthProvider } from "@/contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <LanguageProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </LanguageProvider>
   </React.StrictMode>
 );

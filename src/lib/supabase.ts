12338bc587d5afe4bbf35eab940efda2fd2f8f29
gb05313594187-tech <gb05313594187@gmail.com>
2026-02-21 09:04:38 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,9 @@
 // src/lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
+console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("SUPABASE KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY?.slice(0, 20));
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
 
